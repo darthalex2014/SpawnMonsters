01738eec9357e060a19994a03e0e4be02944573c
Berisan <berisan@berisan.dev>
2024-04-09 21:40:25 +0200

    Overhaul MonsterData Dictionary initialization

--- Spawn Monsters/MonsterSpawning/Spawner.cs
+++ Spawn Monsters/MonsterSpawning/Spawner.cs
@@ -5,6 +5,7 @@ using StardewValley;
 using StardewValley.Monsters;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Spawn_Monsters.MonsterSpawning
 {
@@ -51,14 +52,18 @@ namespace Spawn_Monsters.MonsterSpawning
             //spawn monster
             if (IsOkToPlace(monster, location)) {
                 MonsterData monsterData = MonsterData.GetMonsterData(monster);
-                object[] args = monsterData.ConstructorArgs;
+                
 
-                if (monster == MonsterData.Monster.BlackSlime) {
-                    args[1] = new Color(40 + random.Next(10), 40 + random.Next(10), 40 + random.Next(10));
+                IEnumerable<object> args = new object[] { location };
+                if(monsterData.SecondConstructorArg != null) {
+                    args = args.Append( monsterData.SecondConstructorArg);
                 }
 
-                args[0] = location;
-                Monster m = (Monster) Activator.CreateInstance(monsterData.Type, args);
+                if (monster == MonsterData.Monster.BlackSlime) {
+                    args = args.Append(new Color(40 + random.Next(10), 40 + random.Next(10), 40 + random.Next(10)));
+                }
+
+                Monster m = (Monster) Activator.CreateInstance(monsterData.Type, args.ToArray());
                 m.currentLocation = Game1.currentLocation;
 
                 if (monster == MonsterData.Monster.GraySlime) {
